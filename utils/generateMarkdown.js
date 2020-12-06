d65766745c3725533fea1811500029863f3f0287
ademir <60299587+alidemirbay@users.noreply.github.com>
2020-12-06 12:37:14 -0800

    badge image updated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,31 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
-[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/${data.userName}/${data.title})
-# Description
+  return `# ${data.title}  
+![GitHub license](https://img.shields.io/badge/license-${data.license}-brightgreen)
+
+## Description
 ${data.description}
-# Table of Contents 
+
+## Table of Contents 
 * [Installation](#installation)
 * [Usage](#usage)
 * [Contributing](#contributing)
 * [Tests](#tests)
 * [License](#license)
-# Installation
-The following necessary dependencies must be installed to run the application properly: ${data.installation}
-# Usage
+
+## Installation
+The following  dependencies must be installed to run the application: ${data.installation}
+
+## Usage
 ​This application is used for ${data.usage}
-# License
-This project is license under the ${data.license} license.
-# Contributing
+
+## License
+This project is under the ${data.license} license.
+
+## Contributing
 ​Contributors: ${data.contributing}
-# Tests
+
+## Tests
 To run tests, you need to run the following command: ${data.test}
 `;
 
